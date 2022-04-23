e83152981b40c699e70c82e1f2f1c9796ca0eb4d
Kugelek <poblocki.jakub@gmail.com>
2022-04-23 13:46:31 +0200

    turn off db temporarily

--- kubernetes-performance-back/src/main/java/com/kubperf/kubernetesperformanceback/KubernetesPerformanceBackApplication.java
+++ kubernetes-performance-back/src/main/java/com/kubperf/kubernetesperformanceback/KubernetesPerformanceBackApplication.java
@@ -2,12 +2,12 @@ package com.kubperf.kubernetesperformanceback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//todo: wyrzucic exclude po ogarnieciu bazki
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class KubernetesPerformanceBackApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KubernetesPerformanceBackApplication.class, args);
 	}
-
 }
