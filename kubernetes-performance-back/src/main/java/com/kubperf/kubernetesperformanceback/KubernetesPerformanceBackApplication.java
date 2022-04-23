ce456b8218f0184526145444d9c688970a00ef8c
Kugelek <poblocki.jakub@gmail.com>
2022-04-23 13:37:28 +0200

    spring init

---
+++ kubernetes-performance-back/src/main/java/com/kubperf/kubernetesperformanceback/KubernetesPerformanceBackApplication.java
@@ -0,0 +1,13 @@
+package com.kubperf.kubernetesperformanceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesPerformanceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesPerformanceBackApplication.class, args);
+	}
+
+}
---
+++ kubernetes-performance-back/src/test/java/com/kubperf/kubernetesperformanceback/KubernetesPerformanceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubperf.kubernetesperformanceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesPerformanceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
