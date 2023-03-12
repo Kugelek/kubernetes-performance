11bbb4bd79efbd90ecddbfcc2316a65880d6a076
Kugelek <poblocki.jakub@gmail.com>
2023-03-12 14:18:53 +0100

    jaeger setup, spans example

--- kubernetes-performance-back/src/main/java/com/kubperf/kubernetesperformanceback/KubernetesPerformanceBackApplication.java
+++ kubernetes-performance-back/src/main/java/com/kubperf/kubernetesperformanceback/KubernetesPerformanceBackApplication.java
@@ -3,6 +3,9 @@ package com.kubperf.kubernetesperformanceback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 ////todo: wyrzucic exclude po ogarnieciu bazki
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
@@ -12,5 +15,10 @@ public class KubernetesPerformanceBackApplication {
 	// test comment
 	public static void main(String[] args) {
 		SpringApplication.run(KubernetesPerformanceBackApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 }
--- kubernetes-performance-back/src/main/java/com/kubperf/kubernetesperformanceback/controllers/SpotifyController.java
+++ kubernetes-performance-back/src/main/java/com/kubperf/kubernetesperformanceback/controllers/SpotifyController.java
@@ -8,10 +8,11 @@ import net.minidev.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
@@ -21,7 +22,31 @@ public class SpotifyController {
         public SpotifyService spotifyService;
 
 
+
+
         final static Logger logger = LoggerFactory.getLogger(SpotifyController.class);
+
+
+
+        private RestTemplate restTemplate;
+
+//        @Value("${spring.application.name}")
+//        private String applicationName;
+
+        public  SpotifyController(RestTemplate restTemplate) {
+                this.restTemplate = restTemplate;
+        }
+        @GetMapping("/path1")
+        public ResponseEntity<String> path1() {
+                logger.info("Incoming request at {} for request /path1 ", "app1");
+                String response = restTemplate.getForObject("http://localhost:8090/path2", String.class);
+                return ResponseEntity.ok("response from /path1 + " + response);
+        }
+        @GetMapping("/path2")
+        public ResponseEntity<String>  path2() {
+                logger.info("Incoming request at {} at /path2", "app2");
+                return ResponseEntity.ok("response from /path2 ");
+        }
 
         @GetMapping("/playlists")
         public JSONObject fetchPlaylists() {
